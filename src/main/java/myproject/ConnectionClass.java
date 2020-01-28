d58802535ff5bc9aaa8ab2fe5dfe7ddf19f60c7c
rchrist2 <rachelc504@gmail.com>
2020-01-28 14:41:08 -0600

    added connection class that uses application.properties file for user/pass

--- src/main/java/myproject/ConnectionClass.java
+++ src/main/java/myproject/ConnectionClass.java
@@ -2,18 +2,26 @@ package myproject;
 
 import org.springframework.stereotype.Component;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 @Component
 public class ConnectionClass {
     public static Connection connect(){
         Connection c = null;
         String url = "jdbc:sqlserver://127.0.0.1\\SQLEXPRESS;database=4375db";
+        Properties props= new Properties();
+        FileInputStream input = null;
         try
         {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            c = DriverManager.getConnection(url, "sa", "1234");
+            input = new FileInputStream("src/main/resources/application.properties");
+            props.load(input );
+            c = DriverManager.getConnection(props.getProperty("spring.datasource.url"),
+                    props.getProperty("spring.datasource.username"),
+                    props.getProperty("spring.datasource.password"));
         }
         catch(Exception e)
         {
