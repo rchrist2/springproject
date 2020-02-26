1b82d0209506aa5e22034976cf4368f1e77e7c6f
nguyenkevin96 <47707144+nguyenkevin96@users.noreply.github.com>
2020-02-25 19:18:31 -0600

    UI changes for the scheduler

---
+++ src/main/java/myproject/controllers/EmployeeScheduler.java
@@ -0,0 +1,15 @@
+package myproject.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeeScheduler implements Initializable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
