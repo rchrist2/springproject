45245dccb559e2298ba1c7dbb99000563b5fd8c6
rchrist2 <rachelc504@gmail.com>
2020-02-28 20:40:53 -0600

    fixed threading issues in forgot password page, customized email message and added error messages

--- src/main/java/myproject/controllers/ForgotPassController.java
+++ src/main/java/myproject/controllers/ForgotPassController.java
@@ -1,5 +1,11 @@
 package myproject.controllers;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,6 +15,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
+import javafx.util.Duration;
 import myproject.ErrorMessages;
 import myproject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -69,72 +76,108 @@ public class ForgotPassController implements Initializable {
 
     //Follows example here: https://www.baeldung.com/spring-email
     @FXML
-    private void sendCode() throws IOException, MessagingException {
-        //currently not working, likely due to window freezing on button click
-        //moving to separate method or using Thread.sleep does not work
-        /*Label lb1 = new Label("Sending email...");
-        paneLoadTextfield.getChildren().add(lb1);*/
+    private void sendCode() throws IOException, MessagingException, InterruptedException {
+        Label lb1 = new Label("Sending email...");
+        paneLoadTextfield.getChildren().add(lb1);
 
+        //if the user exists in the database, then email the code to them
         if(userRepository.findUsername(signupUsernameText.getText()) != null) {
             String userEmail = signupUsernameText.getText();
             forgotPassUser = userEmail;
 
+            //save the user's name for the email message
+            String userEmpName = userRepository.findUsername(userEmail).getEmployee().getName();
+
+            //generate the reset confirmation code
             String resetCode = getAlphaNumericString(5);
 
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(userEmail);
-            message.setSubject("Test from Spring Boot");
-            message.setText(resetCode);
-            emailSender.send(message);
+            //start a background thread to send the mail
+            //this allows a "Sending email..." label to be shown while the user waits
+            Thread thread = new Thread(() -> {
+                try {
+                    //send the email
+                    SimpleMailMessage message = new SimpleMailMessage();
+                    message.setTo(userEmail);
+                    message.setSubject("Password Reset Confirmation");
+                    message.setText("Hello " + userEmpName + ","
+                            + "\n\nYou have requested to reset your account password."
+                            + "\n\nYour confirmation code is: " + resetCode
+                            + "\n\nIf you did not request this email, please ignore this message."
+                            + "\n\nSincerely,"
+                            + "\nSpace City Solutions"
+                            + "\nHouston, TX 77583");
+                    emailSender.send(message);
 
-            //maybe change this to a pop-up message or label
-            System.out.println("Email sent");
-            ErrorMessages.showInformationMessage("Success", "Successfully sent email", "Email sent");
+                    //show the rest of the UI elements to validate the code that was emailed
+                    Platform.runLater(new Runnable() {
+                        public void run() {
+                            //maybe change this to a pop-up message or label
+                            System.out.println("Email sent");
+                            ErrorMessages.showInformationMessage("Success", "Successfully sent email", "Email sent");
 
-            //paneLoadTextfield.getChildren().remove(lb1);
+                            paneLoadTextfield.getChildren().remove(lb1);
 
-            TextField tf1 = new TextField();
-            Button button = new Button();
+                            TextField tf1 = new TextField();
+                            Button button = new Button();
 
-            paneLoadTextfield.getChildren().add(tf1);
-            paneLoadButton.setVisible(true);
-            paneLoadButton.getChildren().add(button);
-            signupButton.setVisible(false);
+                            paneLoadTextfield.getChildren().add(tf1);
+                            paneLoadButton.setVisible(true);
+                            paneLoadButton.getChildren().add(button);
+                            signupButton.setVisible(false);
 
-            tf1.setPrefWidth(signupUsernameText.getPrefWidth());
-            tf1.setPrefHeight(signupUsernameText.getPrefHeight());
-            tf1.setPromptText("Enter code sent to email");
+                            tf1.setPrefWidth(signupUsernameText.getPrefWidth());
+                            tf1.setPrefHeight(signupUsernameText.getPrefHeight());
+                            tf1.setPromptText("Enter code sent to email");
 
-            button.setPrefSize(paneLoadButton.getPrefWidth(), paneLoadButton.getPrefHeight());
-            button.setText("Submit Code");
-            button.setStyle("-fx-background-color: #de1c00; " +
-                    "-fx-font-style: italic; -fx-font-weight: bold; -fx-font-size: 18; -fx-font: System");
+                            button.setPrefSize(paneLoadButton.getPrefWidth(), paneLoadButton.getPrefHeight());
+                            button.setText("Submit Code");
+                            button.setStyle("-fx-background-color: #de1c00; " +
+                                    "-fx-font-style: italic; -fx-font-weight: bold; -fx-font-size: 18; -fx-font: System");
 
-            button.setOnAction(event -> {
-                if (tf1.getText().equals(resetCode)) {
-                    //open replace password page
-                    try {
-                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/ReplacePassView.fxml"));
-                        fxmlLoader.setControllerFactory(springContext::getBean);
-                        Pane pane = fxmlLoader.load();
+                            button.setOnAction(event -> {
+                                if (tf1.getText().equals(resetCode)) {
+                                    //open replace password page
+                                    try {
+                                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/ReplacePassView.fxml"));
+                                        fxmlLoader.setControllerFactory(springContext::getBean);
+                                        Pane pane = fxmlLoader.load();
 
-                        signupAnchor.getChildren().setAll(pane);
-                    } catch (IOException io) {
-                        io.printStackTrace();
-                    }
-                } else {
-                    ErrorMessages.showErrorMessage("Code mismatch", "Code does not match",
-                            "Please re-enter code");
+                                        signupAnchor.getChildren().setAll(pane);
+                                    } catch (IOException io) {
+                                        io.printStackTrace();
+                                    }
+                                } else {
+                                    ErrorMessages.showErrorMessage("Code mismatch", "Code does not match",
+                                            "Please re-enter code");
+                                }
+
+                            });
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace(); //can remove this if we don't want errors in console
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            paneLoadTextfield.getChildren().remove(lb1);
+                            ErrorMessages.showErrorMessage("Failed to send email", "Email could not be sent",
+                                    "Please retry or contact administrator");
+                        }
+                    });
                 }
-                ;
             });
+
+            thread.start();
         }
         else{
+            paneLoadTextfield.getChildren().remove(lb1);
             ErrorMessages.showErrorMessage("User does not exist","No user exists with this email",
                     "Please enter a valid email address");
+            }
+
         }
 
-    }
+
 
         //maybe move this to a different class
     @Bean
