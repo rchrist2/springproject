43adc9cd7f66c2bc847acdf0f678c1451ad73479
Kevin Nguyen <kknguy24@gmail.com>
2020-04-07 22:07:39 -0500

    Password requirement for replace password

--- src/main/java/myproject/controllers/WelcomeLoginSignup/ReplacePassController.java
+++ src/main/java/myproject/controllers/WelcomeLoginSignup/ReplacePassController.java
@@ -3,11 +3,16 @@ package myproject.controllers.WelcomeLoginSignup;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.util.Pair;
 import myproject.ErrorMessages;
 import myproject.SecurePassword;
+import myproject.Validation;
 import myproject.models.Tblusers;
 import myproject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +36,9 @@ public class ReplacePassController implements Initializable {
     @FXML
     private TextField newPassConfirmInput;
 
+    @FXML
+    private Tooltip passwordRequirementTooltip;
+
     @Autowired
     private ConfigurableApplicationContext springContext;
 
@@ -39,7 +47,13 @@ public class ReplacePassController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        passwordRequirementTooltip.setFont(Font.font("System", 12));
+        passwordRequirementTooltip.setText("- Password must be 8 to 30 characters long\n" +
+                "- Password must contain the following characters\n" +
+                "\t- Uppercase characters [A-Z]\n" +
+                "\t- Lowercase characters [a-z]\n" +
+                "\t- Contains digit [0-9]\n" +
+                "\t- Contains special characters (!, $, #, etc)");
     }
 
     @FXML
@@ -47,36 +61,42 @@ public class ReplacePassController implements Initializable {
         String replacePassUser = ForgotPassController.forgotPassUser;
         Tblusers replacePassUserAcc = userRepository.findUsername(replacePassUser);
 
+        Pair[] passwordError = Validation.validatePasswordRequirement(newPassInput.getText());
+
         if(!(newPassInput.getText().isEmpty() || newPassConfirmInput.getText().isEmpty()))
         {
             if(newPassInput.getText().equals(newPassConfirmInput.getText())) {
-                try{
-                    System.out.println("The both match");
-                    byte[] salt = SecurePassword.getSalt();
-                    String changedPassword = SecurePassword.getSecurePassword(newPassInput.getText(), salt);
+                if(!(Boolean)passwordError[0].getKey()) {
+                    try {
+                        System.out.println("The both match");
+                        byte[] salt = SecurePassword.getSalt();
+                        String changedPassword = SecurePassword.getSecurePassword(newPassInput.getText(), salt);
 
-                    replacePassUserAcc.setSaltPassword(salt);
-                    replacePassUserAcc.setHashedPassword(changedPassword);
+                        replacePassUserAcc.setSaltPassword(salt);
+                        replacePassUserAcc.setHashedPassword(changedPassword);
 
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                }
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    }
 
-                userRepository.save(replacePassUserAcc);
+                    userRepository.save(replacePassUserAcc);
 
-                System.out.println("Do they Match? :" + SecurePassword.checkPassword(replacePassUserAcc.getHashedPassword(),
-                        newPassConfirmInput.getText(), replacePassUserAcc.getSaltPassword()));
+                    System.out.println("Do they Match? :" + SecurePassword.checkPassword(replacePassUserAcc.getHashedPassword(),
+                            newPassConfirmInput.getText(), replacePassUserAcc.getSaltPassword()));
 
-                ErrorMessages.showInformationMessage("Password reset","Password has been successfully reset",
-                        "You will be redirected to sign-in");
-                try {
-                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/signin.fxml"));
-                    fxmlLoader.setControllerFactory(springContext::getBean);
-                    Pane pane = fxmlLoader.load();
+                    ErrorMessages.showInformationMessage("Password reset", "Password has been successfully reset",
+                            "You will be redirected to sign-in");
+                    try {
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/signin.fxml"));
+                        fxmlLoader.setControllerFactory(springContext::getBean);
+                        Pane pane = fxmlLoader.load();
 
-                    signupAnchor.getChildren().setAll(pane);
-                } catch (IOException io) {
-                    io.printStackTrace();
+                        signupAnchor.getChildren().setAll(pane);
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                } else {
+                    ErrorMessages.showErrorMessage("Error", "Invalid values provided", passwordError[0].getValue().toString());
                 }
             }
             else{
