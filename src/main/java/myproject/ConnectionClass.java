7dfdac04073d83995dbd937c4dfa591a10e29c96
rchrist2 <rachelc504@gmail.com>
2020-04-16 15:05:22 -0500

    adjusted file paths due to error in exe files

--- src/main/java/myproject/ConnectionClass.java
+++ src/main/java/myproject/ConnectionClass.java
@@ -2,7 +2,9 @@ package myproject;
 
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
@@ -12,10 +14,10 @@ public class ConnectionClass {
     public static Connection connect(){
         Connection c = null;
         Properties props= new Properties();
-        FileInputStream input = null;
+        BufferedReader input = null;
         try
         {
-            input = new FileInputStream("src/main/resources/application.properties");
+            input = new BufferedReader(new InputStreamReader(ConnectionClass.class.getResourceAsStream("/application.properties")));
             props.load(input );
             c = DriverManager.getConnection(props.getProperty("spring.datasource.url"),
                     props.getProperty("spring.datasource.username"),
